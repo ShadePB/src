60f3b8eb1d8f4f9aa0e5e6e85e63eb8d99afd0ef
Jack - Shade <shade@pbot.org>
2013-05-10 17:22:03 +0100

    Update

---
+++ org/shade/scripts/lrc/misc/MousePaint.java
@@ -0,0 +1,132 @@
+package org.shade.scripts.lrc.misc;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.util.LinkedList;
+
+import org.powerbot.game.api.methods.input.Mouse;
+
+public class MousePaint {
+	 
+    public int waveSize = 0;
+
+    @SuppressWarnings( { "serial" })
+    public class mousePathPoint extends Point {
+
+        private long finishTime;
+        @SuppressWarnings("unused")
+                    private double lastingTime;
+
+        public mousePathPoint(int x, int y, int lastingTime) {
+            super(x, y);
+            this.lastingTime = lastingTime;
+            finishTime = System.currentTimeMillis() + lastingTime;
+        }
+
+        public boolean isUp() {
+            return System.currentTimeMillis() > finishTime;
+        }
+    }
+
+    public double getRot(int ticks) {
+        return (System.currentTimeMillis() % (360 * ticks)) / ticks;
+    }
+
+    public LinkedList<mousePathPoint> mousePath = new LinkedList<>();
+
+    public void drawTrail(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(2));
+        while (!mousePath.isEmpty() && mousePath.peek().isUp()) {
+            mousePath.remove();
+        }
+        Point clientCursor = Mouse.getLocation();
+        mousePathPoint mpp = new mousePathPoint(clientCursor.x,
+                clientCursor.y, 250);
+        if (mousePath.isEmpty() || !mousePath.getLast().equals(mpp)) {
+            mousePath.add(mpp);
+        }
+        mousePathPoint lastPoint = null;
+        for (mousePathPoint a : mousePath) {
+            if (lastPoint != null) {
+                long mpt = System.currentTimeMillis()
+                        - Mouse.getPressTime();
+                if (Mouse.getPressTime() == -1 || mpt >= 250) {
+                    g2.setColor(Color.WHITE);
+                }
+                if (mpt < 250) {
+                    g2.setColor(Color.RED);
+                }
+                g2.drawLine(a.x, a.y, lastPoint.x, lastPoint.y);
+            }
+            lastPoint = a;
+        }
+    }
+
+    public void drawMouse(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHints(new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON));
+        g2.setStroke(new BasicStroke(3));
+        g2.setColor(Color.BLACK);
+        g2.drawOval(Mouse.getLocation().x - 8, Mouse.getLocation().y - 8,
+                15, 15);
+        g2.setStroke(new BasicStroke(1));
+        g2.setColor(new Color(0, 0, 0, 114));
+        g2.fillOval(Mouse.getLocation().x - 8, Mouse.getLocation().y - 8,
+                15, 15);
+        Point MouseLocation = Mouse.getLocation();
+        long mpt = System.currentTimeMillis() - Mouse.getPressTime();
+        g2.rotate(Math.toRadians(getRot(5)), Mouse.getLocation().x, Mouse
+                .getLocation().y);
+        if (Mouse.getPressTime() == -1 || mpt >= 250) {
+            g2.setColor(Color.CYAN);
+            g2.drawLine(MouseLocation.x - 4, MouseLocation.y,
+                    MouseLocation.x + 4, MouseLocation.y);
+            g2.drawLine(MouseLocation.x, MouseLocation.y - 4,
+                    MouseLocation.x, MouseLocation.y + 4);
+        }
+        if (mpt < 250) {
+            g2.setColor(Color.RED);
+            g2.drawLine(MouseLocation.x - 5, MouseLocation.y,
+                    MouseLocation.x + 5, MouseLocation.y);
+            g2.drawLine(MouseLocation.x, MouseLocation.y - 5,
+                    MouseLocation.x, MouseLocation.y + 5);
+        }
+    }
+
+    public void drawWave(Graphics g, Color color) {
+        Graphics2D g2 = (Graphics2D) g;
+        Point MouseLoc = new Point(Mouse.getPressX(), Mouse.getPressY());
+        long mpt = System.currentTimeMillis() - Mouse.getPressTime();
+        g2.setRenderingHints(new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON));
+        g2.setColor(color);
+        if (mpt < 600) {
+            waveSize = (int) (mpt / 7.5);
+        } else {
+            waveSize = 0;
+        }
+        g2.setStroke(new BasicStroke(1));
+        g2.drawOval(MouseLoc.x - (waveSize / 2), MouseLoc.y
+                - (waveSize / 2), waveSize, waveSize);
+        g2.drawOval(MouseLoc.x - ((waveSize / 2) / 2), MouseLoc.y
+                - ((waveSize / 2) / 2), waveSize / 2, waveSize / 2);
+    }
+
+    public void Draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHints(new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON));
+        drawWave(g2, Color.BLACK);
+        drawTrail(g2);
+        drawMouse(g2);
+    }
+}
---
+++ org/shade/scripts/lrc/misc/Variables.java
@@ -0,0 +1,45 @@
+package org.shade.scripts.lrc.misc;
+
+import org.powerbot.game.api.wrappers.Tile;
+
+public class Variables {
+	
+	//SceneObject IDs
+	public static final int COAL_DEPOSIT_ID = 5999;
+    public static final int GOLD_DEPOSIT_ID = 45076;
+    public static final int[] BOTH_DEPOSITS_ID = { 5999, 45076 };
+    public static final int TROLLEY_ID = 45079;
+	
+	//Item IDs
+    public static final int GOLD_ORE_ID = 444;
+    public static final int COAL_ORE_ID = 453;
+    public static final int[] SUMMONING_POTION_ID = { 12140, 12142, 12144, 12146 };
+    public static final int[] PICKAXE_IDS = { 15259, 1275 };
+    public static final int OBSIDIAN_GOLEM_POUCH_ID = 12792;
+    public static final int LAVA_TITAN_POUCH_ID = 12788;
+    public static final int[] ALL_POUCHES_ID = { 12792, 12788 };
+    public static final int FINISHED_URN_ID = 20408;
+    public static final int URN_ID = 20406 ;
+    public static final int CURRENT_URN_ID = 20407 ;
+	
+	//Other
+    public static final Tile WEST_TILE = new Tile(3638, 5095, 0);
+    public static final Tile EAST_TILE = new Tile(3668, 5076, 0);
+    public static final Tile COAL_TILE = new Tile(3665, 5091, 0);
+    public static final Tile COAL_TILE_2 = new Tile(3675, 5099, 0);
+
+    public static boolean banking = true;
+    public static boolean pickaxeEquipped = true;
+    public static boolean usingSummoning = false;
+    public static boolean usingUrns = false;
+    public static boolean needToBank = false;
+	
+	//Paint
+    public static String status = "Welcome";
+    public static int startExp = 0;
+    public static int orePrice = 0;
+    public static int oresMined = 0;
+    public static long startTime = 0;
+	
+	
+}
